df262baf0acaf02f0b6aadd31f947eff9d9411a4
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2018-05-19 22:38:35 +0200

    Add clj-http, cheshire and cleanup project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.908"]
                  [reagent "0.7.0"]
-                 [re-frame "0.10.5"]]
+                 [re-frame "0.10.5"]
+                 [clj-http "3.9.0"]
+                 [cheshire "5.8.0"]]
 
   :plugins [[lein-cljsbuild "1.1.5"]]
 
@@ -14,36 +16,26 @@
 
   :figwheel {:css-dirs ["resources/public/css"]}
 
-  :profiles
-  {:dev
-   {:dependencies [[binaryage/devtools "0.9.4"]]
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.4"]]
 
-    :plugins      [[lein-figwheel "0.5.13"]]}
-   :prod { }}
+                   :plugins      [[lein-figwheel "0.5.13"]]}}
 
-  :cljsbuild
-  {:builds
-   [{:id           "dev"
-     :source-paths ["src/cljs"]
-     :figwheel     {:on-jsload "eline_proj.core/mount-root"}
-     :compiler     {:main                 eline_proj.core
-                    :output-to            "resources/public/js/compiled/app.js"
-                    :output-dir           "resources/public/js/compiled/out"
-                    :asset-path           "js/compiled/out"
-                    :source-map-timestamp true
-                    :preloads             [devtools.preload]
-                    :external-config      {:devtools/config {:features-to-install :all}}
-                    }}
+  :cljsbuild {:builds
+              [{:id           "dev"
+                :source-paths ["src/cljs"]
+                :figwheel     {:on-jsload "eline_proj.core/mount-root"}
+                :compiler     {:main                 eline_proj.core
+                               :output-to            "resources/public/js/compiled/app.js"
+                               :output-dir           "resources/public/js/compiled/out"
+                               :asset-path           "js/compiled/out"
+                               :source-map-timestamp true
+                               :preloads             [devtools.preload]
+                               :external-config      {:devtools/config {:features-to-install :all}}}}
 
-    {:id           "min"
-     :source-paths ["src/cljs"]
-     :compiler     {:main            eline_proj.core
-                    :output-to       "resources/public/js/compiled/app.js"
-                    :optimizations   :advanced
-                    :closure-defines {goog.DEBUG false}
-                    :pretty-print    false}}
-
-
-    ]}
-
-  )
+               {:id           "min"
+                :source-paths ["src/cljs"]
+                :compiler     {:main            eline_proj.core
+                               :output-to       "resources/public/js/compiled/app.js"
+                               :optimizations   :advanced
+                               :closure-defines {goog.DEBUG false}
+                               :pretty-print    false}}]})
