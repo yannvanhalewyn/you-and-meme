9c76d761d624e5e32a412c9686e68f055d12aaf0
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2018-05-19 22:51:20 +0200

    Add youtube ns for fetching random videos

--- src/clj/eline_proj/core.clj
+++ src/clj/eline_proj/core.clj
@@ -1,1 +1,8 @@
-(ns eline-proj.core)
+(ns eline-proj.core
+  (:gen-class)
+  (:require [eline-proj.youtube :as yt]
+            [eline-proj.download :refer [download]]))
+
+(defn -main [& args]
+  (let [v (yt/get-random-video)]
+    (download (:video/id v) "videos/")))
---
+++ src/clj/eline_proj/youtube.clj
@@ -0,0 +1,27 @@
+(ns eline-proj.youtube
+  (:require [clj-http.client :as http]))
+
+(def API_KEY "***")
+
+(defn- rand-str [size]
+  (let [chars (map char (concat (range 48 58) (range 65 91)))]
+    (apply str (take size (repeatedly #(rand-nth chars))))))
+
+(def URL_FMT  "https://www.googleapis.com/youtube/v3/search?key=%s&maxResults=%s&part=snippet&type=video&q=%s")
+
+(def url #(format URL_FMT API_KEY %1 %2))
+
+(defn- get-random-videos [count]
+  (let [q (rand-str 3)]
+    (println q)
+    (get-in
+     (http/get (url 20 q) {:accept :json :as :json})
+     [:body :items])))
+
+(defn- parse-item [{:keys [snippet id]}]
+  {:video/id (:videoId id)
+   :video/title (:title snippet)
+   :video/thumb (get-in (:thumbnails snippet) [:high :url])})
+
+(defn- get-random-video []
+  (parse-item (first (get-random-videos 1))))
