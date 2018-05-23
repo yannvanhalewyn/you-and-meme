c58afee7232ee49647b1af0767054411efc8f9eb
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2018-05-23 21:13:29 +0200

    Pretty print the host connection url (tablets / phones) on startup

--- src/clj/eline_proj/core.clj
+++ src/clj/eline_proj/core.clj
@@ -1,9 +1,20 @@
 (ns eline-proj.core
   (:gen-class)
+  (:import [java.net InetAddress])
   (:require [eline-proj.system :refer [new-system]]
             [com.stuartsierra.component :as c]
-            [clojure.java.browse :refer [browse-url]]))
+            [clojure.java.browse :refer [browse-url]]
+            [clojure.string :as str]))
+
+(defn- host-url-connection-message []
+  (let [local-ip (.getHostAddress (InetAddress/getLocalHost))
+        line (format "== Verbind je tablet of telefoon op: http://%s:8080 =="
+                     local-ip)
+        sep-outer (str/join (repeat (count line) \=))
+        sep-inner (format "==%s==" (str/join (repeat (- (count line) 4) " ")))]
+    (str/join "\n" [sep-outer sep-inner line sep-inner sep-outer])))
 
 (defn -main [& args]
   (c/start (new-system))
+  (println (host-url-connection-message))
   (browse-url "http://localhost:8080"))
